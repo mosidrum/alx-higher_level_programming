fd76f16cb51925f2e7662c18b4bf77353a8f3ef8
mosidrum <mosiokanga@gmail.com>
2022-09-23 02:10:37 +0100

    bless up

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for char in range(ord("a"), ord("z") + 1):
-    print("{:c}".format(char), end='')"\n"
+for a in range(ord('a'), ord('z') + 1):
+    print("{:c}".format(a), end='')
