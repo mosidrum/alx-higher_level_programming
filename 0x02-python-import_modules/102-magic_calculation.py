f89cfac7737f8f58056887daa3b42b819d7153be
mosidrum <mosiokanga@gmail.com>
2022-09-22 20:27:42 -0700

    omo nawa ogit

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -7,13 +7,4 @@ def magic_calculator(a,b):
             c = add(c, i)
         return c
     else:
-        return sub(a, b)#!/usr/bin/python3
-def magic_calculator(a, b)
-    from magic_calculator import add, sub
-    if a > b:
-        c = add(a, b)
-        for i in range(4, 6):
-            c = add(c, i)
-        return c
-    else:
         return sub(a, b)
